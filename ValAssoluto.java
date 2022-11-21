485742cea1c7dc2531e2650a0e7a2e3bf5747824
Gianluca <gl.galassini@gmail.com>
2022-11-21 18:07:28 +0100

    Valore Assoluto done

---
+++ ValAssoluto.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ValAssoluto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("inserisci un numero");
+        int num = sc.nextInt();
+        if (num < 0) {
+            System.out.println(num * -1);
+        } else {
+            System.out.println(num);
+        }
+    }
+}
