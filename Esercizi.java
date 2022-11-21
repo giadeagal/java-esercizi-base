270066d0a8ebd3d384ac40d0ffc7b5fecfcd706a
Gianluca <gl.galassini@gmail.com>
2022-11-21 12:17:49 +0100

    fibonacci

--- Esercizi.java
+++ Esercizi.java
@@ -1,6 +1,12 @@
 public class Esercizi {
-
     public static void main(String[] args) {
-        
+        int numGiri = 5;
+        int numPartenza = 0;
+        int risultato = 0;
+
+        for(int i = numPartenza; i > numGiri; i++ ) {
+            System.out.println(risultato+i);
+            risultato += i;
+        }
     }
 }
