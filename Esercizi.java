756a02cb7ccfa83de4561f0f39e9ff223b50c016
Gianluca <gl.galassini@gmail.com>
2022-11-21 13:11:55 +0100

    Fibonacci metodi alternativi

--- Esercizi.java
+++ Esercizi.java
@@ -1,5 +1,48 @@
 public class Esercizi {
+    static void fibonacciamo(int laps, int num) {
+        int num1 = num-1;
+        int num2 = num-2;
+        int result;
+
+        for( int i = 0; i < laps; i++) {
+            if (i == 0 && num == 0) {
+                System.out.print(0 + " " + 1);
+            } else {
+                result = num1 + num2;    
+            System.out.print(" "+ result);    
+            num1 = num2;    
+            num2 = result; 
+            }
+        }
+
+        System.out.println();
+      }
+
+      static void fibonacciamo(int laps) {
+        int num1 = 0;
+        int num2 = 1;
+        int result;
+
+        for( int i = 0; i < laps; i++) {
+            if (i == 0) {
+                System.out.print(0 + " " + 1);
+            } else {
+                result = num1 + num2;    
+            System.out.print(" "+ result);    
+            num1 = num2;    
+            num2 = result; 
+            }
+        }
+        System.out.println();
+      }
     public static void main(String[] args) {
+        //Con funzione a 2 argomenti (lunghezza e numero di partenza)
+        fibonacciamo(5, 6);
+
+        //Con funzione ad argomento singolo (lunghezza, partendo da 0)
+        fibonacciamo(5);
+
+        //Senza funzione
         int laps = 20;
         int num1 = 0;
         int num2 = 1;
@@ -13,6 +56,5 @@ public class Esercizi {
             System.out.print(" "+ result);    
             num1 = num2;    
             num2 = result;  
-        }
-    }
-}
+        } 
+    }}
