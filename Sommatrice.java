4b5160a8e0144b37dc32fcbc22f1f46db24c8e65
Gianluca <gl.galassini@gmail.com>
2022-11-21 17:43:36 +0100

    sommatrice in corso

---
+++ Sommatrice.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Sommatrice {
+    public static void main(String[] args) {
+        sum();
+    }
+
+    public static void sum() {
+        int num = getNumber();
+        int tot;
+        getNumber();
+        }
+
+    public static int getNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserisci un numero");
+        int num = sc.nextInt();
+        return num;
+    }
+    }
+
