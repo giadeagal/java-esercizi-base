dfb5efbf85dbde405a1acd84187fea73368e1ca7
Gianluca <gl.galassini@gmail.com>
2022-11-21 18:22:15 +0100

    Sommatrice done

--- Sommatrice.java
+++ Sommatrice.java
@@ -1,24 +1,21 @@
 import java.util.Scanner;
 
 public class Sommatrice {
-    public static void main(String[] args) {
-        sum();
-    }
 
-    public static void sum() {
-        int num = getNumber();
-        int tot;
-        getNumber();
+   public static void main(String[] args) {
+        boolean continua = true;
+        int num;
+        int tot = 0; 
+        while(continua) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Inserisci un numero");
+            num = sc.nextInt();
+            if (num == 0) {
+                continua = false;
+                System.out.println("totale = " + tot);
+            } else {
+                tot += num;
+            }
         }
-
-    public static int getNumber() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Inserisci un numero");
-        int num = sc.nextInt();
-        if (num == 0) {
-            
-        }
-        return num;
-    }
-    }
-
+   } 
+}
