62509e1615fbd12f3502e2e74310c57bcdd1abb3
Gianluca <gl.galassini@gmail.com>
2022-11-22 16:39:49 +0100

    versione statica

--- DiagonalMatrix.java
+++ DiagonalMatrix.java
@@ -1,27 +1,33 @@
 public class DiagonalMatrix {
     public static void main(String[] args) {
-        diagonalMatrixChar(3, 'A');
+        diagonalMatrixChar(5, 'X');
     }
 
-    public static void diagonalMatrixChar(int sideLength, char letter) {
+    public static char[][] diagonalMatrixChar(int sideLength, char letter) {
         char[][] matrix = new char[sideLength][sideLength];
-
-        //Posizioni X e Y in cui stampare il carattere
-        int char1PosY = 0;
-        int char1PosX = 0;
-        
-        
 
         //Scorro le righe
         for (int x = 0; x < sideLength; x++ ) {
-
-            //Ad ogni riga scorro le colonne
+            
+            //Ad ogni riga scorro le colonne e scrivo
             for (int y = 0; y < sideLength; y++) {
-                
+                if (y == 0 && x == 0 ||
+                    y == 0 && x == 4 ||
+                    y == 1 && x == 1 ||
+                    y == 1 && x == 3 ||
+                    y == 2 && x == 2 ||
+                    y == 3 && x == 1 ||
+                    y == 3 && x == 3 ||
+                    y == 4 && x == 0 ||
+                    y == 4 && x == 4) {
+                        System.out.print(" " + letter + " ");
+                    } else  {
+                        System.out.print(" 0 ");
+                    }
             }
             System.out.println("");
             
         }
-
+        return matrix;
     }
 }
