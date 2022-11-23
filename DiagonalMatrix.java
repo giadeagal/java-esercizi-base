fa3a8542b1e8c0d1d06a7fbd7350b59d85004b31
Gianluca <gl.galassini@gmail.com>
2022-11-23 11:58:27 +0100

    funzione dinamica

--- DiagonalMatrix.java
+++ DiagonalMatrix.java
@@ -1,6 +1,6 @@
 public class DiagonalMatrix {
     public static void main(String[] args) {
-        diagonalMatrixChar(5, 'X');
+        diagonalMatrixChar(10, '|');
     }
 
     public static char[][] diagonalMatrixChar(int sideLength, char letter) {
@@ -11,18 +11,10 @@ public class DiagonalMatrix {
             
             //Ad ogni riga scorro le colonne e scrivo
             for (int y = 0; y < sideLength; y++) {
-                if (y == 0 && x == 0 ||
-                    y == 0 && x == 4 ||
-                    y == 1 && x == 1 ||
-                    y == 1 && x == 3 ||
-                    y == 2 && x == 2 ||
-                    y == 3 && x == 1 ||
-                    y == 3 && x == 3 ||
-                    y == 4 && x == 0 ||
-                    y == 4 && x == 4) {
+                if (x == y || x + y == sideLength-1) {
                         System.out.print(" " + letter + " ");
                     } else  {
-                        System.out.print(" 0 ");
+                        System.out.print(" O ");
                     }
             }
             System.out.println("");
